505c4f7a277e7d0a35c21d6e4e81be3b55a5659b
Youssef Ali Saber <ya1654@fayoum.edu.eg>
2024-11-30 16:36:04 +0200

    add get customer list endpoint

---
+++ WebApi/Endpoints/CustomerEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using WebApi.Factory;
+using WebApi.Models;
+
+namespace WebApi.Endpoints;
+
+public static class CustomerEndpoints
+{
+    public static void MapCustomerEndpoints(this WebApplication app)
+    {
+        app.MapGet("customers", async (SqlConnectionFactory sqlConnectionFactory) =>
+        {
+            using var connection = sqlConnectionFactory.Create();
+            const string sql = "SELECT * FROM Customers";
+            var customers = await connection.QueryAsync<Customer>(sql);
+            return Results.Ok(customers);
+        });
+    }
+}
---
+++ WebApi/Factory/SqlConnectionFactory.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace WebApi.Factory;
+
+public class SqlConnectionFactory(string connectionString)
+{
+    public SqlConnection Create()
+    {
+        return new SqlConnection(connectionString);
+    }
+}

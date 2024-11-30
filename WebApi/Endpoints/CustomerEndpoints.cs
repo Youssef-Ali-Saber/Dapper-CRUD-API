5c4efafe2fc4a2f460d9da1631eb6d78cce40408
Youssef Ali Saber <ya1654@fayoum.edu.eg>
2024-11-30 16:37:58 +0200

    add endpoint to get customer by id

--- WebApi/Endpoints/CustomerEndpoints.cs
+++ WebApi/Endpoints/CustomerEndpoints.cs
@@ -15,5 +15,13 @@ public static class CustomerEndpoints
             var customers = await connection.QueryAsync<Customer>(sql);
             return Results.Ok(customers);
         });
+
+        app.MapGet("customers/{id}", async (int id, SqlConnectionFactory sqlConnectionFactory) =>
+        {
+            using var connection = sqlConnectionFactory.Create();
+            const string sql = "SELECT * FROM Customers WHERE Id = @Id";
+            var customer = await connection.QuerySingleOrDefaultAsync<Customer>(sql, new { Id = id });
+            return customer is not null ? Results.Ok(customer) : Results.NotFound();
+        });
     }
 }
